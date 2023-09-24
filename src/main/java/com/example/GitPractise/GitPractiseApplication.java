9cc659500d6dcce76a056d07ed4476d3d9f47c61
unknown <mrkarlhendrik@gmail.com>
2023-09-24 13:12:49 +0300

    Initial commit

---
+++ src/main/java/com/example/GitPractise/GitPractiseApplication.java
@@ -0,0 +1,14 @@
+package com.example.GitPractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractiseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GitPractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitPractise/GitPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitPractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
